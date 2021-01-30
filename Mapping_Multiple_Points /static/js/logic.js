2a09aa8b49140f0c30912bf6c4026a9c8dc40a23
Alexander Klimov <alexanderklimov@Alexanders-MBP.cogeco.local>
2021-01-30 02:24:49 -0500

    commit

--- Mapping_Multiple_Points /static/js/logic.js
+++ Mapping_Multiple_Points /static/js/logic.js
@@ -12,7 +12,13 @@ cities.forEach(function(city) {
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
   console.log(city)
-  L.marker(city.location).addTo(map);
+  L.circleMarker(city.location, {
+    color : "orange",
+    radius : city.population/100000
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString()
+   + "</h3>")
+.addTo(map);
 });
  // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
